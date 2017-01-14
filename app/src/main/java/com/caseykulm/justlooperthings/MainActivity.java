5d0d63a6c1ce5210901c7ff89431a24491bb4b76
Casey Kulm <casey.kulm@bazaarvoice.com>
2017-01-14 12:47:05 -0600

    UI Handler not TextView

--- app/src/main/java/com/caseykulm/justlooperthings/MainActivity.java
+++ app/src/main/java/com/caseykulm/justlooperthings/MainActivity.java
@@ -1,8 +1,8 @@
 package com.caseykulm.justlooperthings;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 import com.caseykulm.looperlib.LibOne;
 import com.caseykulm.looperlib.LibTwo;
@@ -13,25 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        doIt(new Runnable() {
+        Handler uiHandler = new Handler();
+        uiHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 LibOne.getInstance().task1();
             }
         }, 1000);
-        doIt(new Runnable() {
+        uiHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 LibOne.getInstance().task2();
             }
         }, 2000);
-        doIt(new Runnable() {
+        uiHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 LibTwo.getInstance().task1();
             }
         }, 3000);
-        doIt(new Runnable() {
+        uiHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 LibTwo.getInstance().task2();
@@ -39,8 +40,4 @@ public class MainActivity extends AppCompatActivity {
         }, 4000);
     }
 
-    private void doIt(Runnable runnable, long delay) {
-        TextView textView = (TextView) findViewById(R.id.text);
-        textView.postDelayed(runnable, delay);
-    }
 }
